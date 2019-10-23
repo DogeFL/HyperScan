984f2ebc7531e0ea2ab6e3ee31cc3b29a56e3281
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2019-10-22 20:40:33 -0400

    Add a timer and also slave node

--- hyperscanalpha.c
+++ hyperscanalpha.c
@@ -1,9 +1,112 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <mpi.h>
 
 #define MASTER_NODE_RANK 0
+
+
+
+void do_timer(int nNode, char *strEvent, bool bStart, bool bDisplayDuration, double *dStartTime, double *dEndTime, double *dTimeDuration);
+
+void freeMem(int filelen, char ** lines);
+
+char ** createArray(FILE * logFile, char ** lines, int * filelength);
+
+void printArray(char ** lines, int filelength);
+
+
+int main(int argc, char *argv[]) {
+
+  double dTimeDuration=0, dStartTime=0, dEndTime=0;
+
+  int numtasks, taskid, source, dest, nTagArrayOffset, nTagArrayData;
+  char ** lines;
+  FILE * logFile = fopen("log.csv", "r"); // Open our logfile in read mode. 
+  int * filelength = malloc(sizeof(int)); // Allocate some memory for our file size
+
+  // This function creates an array filled with the lines, and also will set the filelength pointer to the length of the file.
+  
+
+MPI_Status status;
+
+MPI_Init(&argc, &argv);
+MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+
+if (taskid == MASTER_NODE_RANK){          //Master Node logic.
+	lines = createArray(logFile, lines, filelength); // We send an empty memory address, so function can fill it in.
+	printf("There are : %d Elements in the array \n", * filelength);
+	printf("I have %d Workers ready to work \n" , numtasks);
+
+
+
+ 	//the first step will be to chop the array into pieces each array can handle.
+
+	int workperNode = (*filelength / numtasks );
+	int workLeft = (*filelength % numtasks );
+		printf("Each node will recieve %d elements to search \n" , workperNode);
+		printf("With %d left over \n" , workLeft);
+
+/*
+int i=0;
+	for (i = 1; i < numtasks; i++) 
+{
+MPI_Send(lines[i], sizeof(greeting), MPI_CHAR,i, 1, MPI_COMM_WORLD, &status);
+printf("%s\n", greeting);
+}
+*/
+
+//Call this after everything is done.
+//freeMem(* filelength,lines); // Deallocate the memory
+
+}
+
+if (taskid > MASTER_NODE_RANK){           // Slave nodes
+
+}
+
+
+MPI_Finalize();
+
+
+//printArray(lines, * filelength);
+
+
+return 0;
+} // End main function
+
+
+
+void do_timer(int nNode, char *strEvent, bool bStart, bool bDisplayDuration, double *dStartTime, double *dEndTime, double *dTimeDuration)
+{
+    if (bStart == true)
+    {
+        // reset duration and end time
+        *dTimeDuration = 0;
+        *dEndTime = 0;
+        
+        // start timer
+        *dStartTime = MPI_Wtime();
+    }
+    else
+    {
+        // stop timer and compute duration
+        *dEndTime = MPI_Wtime();
+        *dTimeDuration = *dEndTime - *dStartTime;
+        
+        // if showing duration, display time it took for event to compute
+        if (bDisplayDuration == true)
+        {
+            // display time duration
+            char strBuf[256];
+            memset(strBuf, 0, 256);
+            sprintf(strBuf, "Time duration for %s from node %d", strEvent, nNode);
+            printf("%-30s=%0.6lf (sec)\n", strBuf, *dTimeDuration);
+        }
+    }
+}
 
 char ** createArray(FILE * logFile, char ** lines, int * filelength) { // Function to get the number of lines, and put them into an array.
 
@@ -56,28 +159,4 @@ void printArray(char ** lines, int filelength) {
 
 }
 
-int main(int argc, char *argv[]) {
-  int numtasks, taskid, source, dest, nTagArrayOffset, nTagArrayData;
-  char ** lines;
-  FILE * logFile = fopen("log.csv", "r"); // Open our logfile in read mode. 
-  int * filelength = malloc(sizeof(int)); // Allocate some memory for our file size
 
-  // This function creates an array filled with the lines, and also will set the filelength pointer to the length of the file.
-  
-MPI_Init(&argc, &argv);
-MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
-if (taskid == MASTER_NODE_RANK){          //Master Node logic.
-	lines = createArray(logFile, lines, filelength);
-	printf("Length: %d", * filelength);
-}
-
-MPI_Finalize();
-
-
-printArray(lines, * filelength);
-
-printf("%d WORLD RANK",taskid);
-
-  freeMem(* filelength,lines);
-}
