49d9bde4b60eb66fdcb79dda358026bae5ad01ce
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2019-11-05 13:26:48 -0500

    Send array as a chunk instead of multiple sends

--- hyperscanalpha.c
+++ hyperscanalpha.c
@@ -20,6 +20,8 @@ void printArray (char **lines, int filelength);
 ///////////////Function to check if attack going on ////////////////////////
 char * scanarray (char **array, int length);
 
+  char ** lines;
+
 int
 main (int argc, char *argv[])
 {
@@ -27,12 +29,13 @@ main (int argc, char *argv[])
   double dTimeDuration = 0, dStartTime = 0, dEndTime = 0;
 
   int numtasks, taskid, source, dest, nTagArrayOffset, nTagArrayData, offset = 0;
-  char **lines;
+
   //MUST BE NAMED LOG.CSV for now
   FILE *logFile = fopen ("log.csv", "r"); // Open our logfile in read mode. 
   int *filelength = malloc (sizeof (int));  // Allocate some memory for our file size
 
   // This function creates an array filled with the lines, and also will set the filelength pointer to the length of the file.
+  //lines = createArray (logFile, lines, filelength); // We send an empty memory address, so function can fill it in.
 
 
   MPI_Status status;
@@ -43,31 +46,48 @@ main (int argc, char *argv[])
   int tag1, tag2, workperNode, workLeft;
   tag2 = 1;
   tag1 = 2;
+  
+      lines = createArray (logFile, lines, filelength); 
 
 ////////////////////Master Node logic/////////////////
 
   if (taskid == MASTER_NODE_RANK)
     {
-
+       //lines = createArray (logFile, lines, filelength);
+int l=0;
       printf ("Array has been initialized...");
-      lines = createArray (logFile, lines, filelength); // We send an empty memory address, so function can fill it in.
       printf ("There are : %d Elements in the array \n", *filelength);
       printf ("I have %d Workers ready to work \n", numtasks);
-
-      //The first step will be to chop the array into pieces each array can handle.
-
+ 
       workperNode = (*filelength / numtasks);
       workLeft = (*filelength % numtasks);
       printf ("Each node will recieve %d elements to search \n", workperNode);
       printf ("With %d left over \n", workLeft);
-      // these are used for deciding what parts of the array to send.
-      int i, start, end;
+      
+      int i, start;
 
+      offset = workperNode;
 
+///////////////////SLAVE WORK///////////////////
+
+      for (dest = 1; dest < numtasks; dest++)
+  {
+    printf ("offset : %d end: %d \n", offset, (workperNode+offset));
+
+        MPI_Send (&workperNode, 1, MPI_INT, dest, 3, MPI_COMM_WORLD);
+        MPI_Send (&offset, 1, MPI_INT, dest, 4, MPI_COMM_WORLD);
+        MPI_Send(&(lines[offset][0]), workperNode, MPI_CHAR, dest, tag2,
+                 MPI_COMM_WORLD);
+        printf("Sent Process: %d array \n",dest);      
+     offset = offset + workperNode ;
+  }
+
+//Call this below when done writing program... 
+//freeMem(* filelength,lines); // Deallocate the memory
+ 
 
       //////////////////MASTER WORK///////////////////
-      printf("MASTER NODE SCANNING:");
-      printf ("PROCESS %d :offset : %d end: %d \n",taskid, offset, workperNode);
+      printf("MASTER NODE SCANNING:\n");
 
       offset = 0;
       for (i = 0; i < workperNode; i++)
@@ -76,33 +96,8 @@ main (int argc, char *argv[])
       {
         printf ("%s \n", scanarray (&lines[i], sizeof (lines[i])));
       }
-  }
 
-
-      end = workperNode * 2;
-      offset = workperNode;
-
-///////////////////SLAVE WORK///////////////////
-
-      for (dest = 1; dest < numtasks; dest++)
-  {
-    printf ("offset : %d end: %d \n", offset, end);
-
-    MPI_Send (&workperNode, 1, MPI_INT, dest, 3, MPI_COMM_WORLD);
-    MPI_Send (&offset, 1, MPI_CHAR, dest, tag1, MPI_COMM_WORLD);
-
-    for (i = offset; i < end - 249; i++)
-      {
-        MPI_Send (&(lines[i][0]), workperNode + 1, MPI_CHAR, dest, tag2,
-      MPI_COMM_WORLD);
-      }
-    offset = offset + workperNode ;
-    end = end + workperNode ;
-  }
-
-//Call this below when done writing program... 
-//freeMem(* filelength,lines); // Deallocate the memory
-
+}
     }
 
 
@@ -113,31 +108,23 @@ main (int argc, char *argv[])
     {
       source = MASTER_NODE_RANK;
 //This will send the buffer size, since the master created it.
+      //Recieve the data from the master.
       MPI_Recv (&workperNode, 1, MPI_INT, source, 3, MPI_COMM_WORLD, &status);
-//MPI_Recv(&offset, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
-      char *buf;
-      buf = malloc (workperNode + 1 * sizeof (char));
-      int i;
-      // The array of results
-      char *resultArr[workperNode + 1 * sizeof (char)];
-      char **temparr; // This is what we will send the the search function
-      temparr = malloc (sizeof (char *) * workperNode);
-      int j = 0; // We can keep track of the bad searches... 
+      MPI_Recv (&offset, 1, MPI_INT, source, 4, MPI_COMM_WORLD, &status);
+           printf ("PROCESS %d :offset : %d end: %d \n",taskid, offset, (workperNode+offset));
+      MPI_Recv (&(lines[offset][0]), workperNode , MPI_CHAR, source, tag2,MPI_COMM_WORLD, &status);
+           printf("PROCESS %d recieved the array........\n", taskid);
 
-      printf("PROCESS %d Scanning in progress.....\n", taskid);
-      for (i = 0; i < workperNode - 249; i++) //IDK, need to find out later why mem address 70k broke...
+int i;
+printf("PROCESS %d Scanning in progress.....\n", taskid);
+ for (i = offset; i < (workperNode + offset); i++)
   {
-    MPI_Recv (buf, workperNode + 1, MPI_CHAR, source, tag2,
-        MPI_COMM_WORLD, &status);
-    temparr[i] = malloc (sizeof (char *) * 300);  //300 = maxlinelen
-    temparr[i] = buf;
-    if (scanarray (temparr, sizeof (temparr[i])) != "not found")
+    if (scanarray (&lines[i], sizeof (lines[i])) != "not found")
       {
-        resultArr[j] = scanarray (temparr, sizeof (temparr[i]));  // This array will hold the results...
-        printf ("PROCESS %d %s \n", taskid, resultArr[j]);          //Print what the node found
-        j++;
+        printf ("%s \n", scanarray (&lines[i], sizeof (lines[i])));
       }
   }
+
     }
   MPI_Finalize ();
 }       // End main function
@@ -246,7 +233,7 @@ char * scanarray (char **array, int length)
 {
   int i;
   char *searcher;   // this is what searches for the strings
-  searcher = strstr (array[0], "0.044947");
+  searcher = strstr (array[0], "1.1.1.1.1");
   if (searcher != NULL)
     {       // This means something is found....
       return array[0];
@@ -254,5 +241,6 @@ char * scanarray (char **array, int length)
   {
     return "not found";
   }
+
 }
 
