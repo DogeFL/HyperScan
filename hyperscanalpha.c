813bea642ce11037600ce3a7889dfa2bde0c0c71
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2019-11-07 18:12:08 -0500

    Create a log file

--- hyperscanalpha.c
+++ hyperscanalpha.c
@@ -6,6 +6,10 @@
 
 #define MASTER_NODE_RANK 0
 
+char **lines = NULL;
+char **outarr;
+int lenoflogFile = 0;
+
 void do_timer (int nNode, char *strEvent, bool bStart, bool bDisplayDuration,
          double *dStartTime, double *dEndTime, double *dTimeDuration);
 ///////////////////////// Free it all ////////////////////////////////////////
@@ -18,9 +22,14 @@ char **createArray (FILE * logFile, char **lines, int *filelength);
 void printArray (char **lines, int filelength);
 
 ///////////////Function to check if attack going on ////////////////////////
-char * scanarray (char **array, int length);
+char *scanarray (char **array, int length);
 
-  char ** lines;
+void allocchar ();
+
+void deleteoldLog ();
+
+void createlogFile (char **outarr, int lenoflogFile);
+
 
 int
 main (int argc, char *argv[])
@@ -28,12 +37,13 @@ main (int argc, char *argv[])
 
   double dTimeDuration = 0, dStartTime = 0, dEndTime = 0;
 
-  int numtasks, taskid, source, dest, nTagArrayOffset, nTagArrayData, offset = 0;
+  int numtasks, taskid, source, dest, nTagArrayOffset, nTagArrayData, offset =
+    0;
 
   //MUST BE NAMED LOG.CSV for now
   FILE *logFile = fopen ("log.csv", "r"); // Open our logfile in read mode. 
   int *filelength = malloc (sizeof (int));  // Allocate some memory for our file size
-
+  int SIZEOFOUTARR = 20000;
   // This function creates an array filled with the lines, and also will set the filelength pointer to the length of the file.
   //lines = createArray (logFile, lines, filelength); // We send an empty memory address, so function can fill it in.
 
@@ -46,24 +56,25 @@ main (int argc, char *argv[])
   int tag1, tag2, workperNode, workLeft;
   tag2 = 1;
   tag1 = 2;
-  
-      lines = createArray (logFile, lines, filelength); 
-
+  lines = createArray (logFile, lines, filelength);
+  outarr = malloc (SIZEOFOUTARR);
 ////////////////////Master Node logic/////////////////
-
   if (taskid == MASTER_NODE_RANK)
     {
-       //lines = createArray (logFile, lines, filelength);
-int l=0;
+      deleteoldLog ();
+      allocchar ();
+      //lines = createArray (logFile, lines, filelength);
+      int l = 0;
       printf ("Array has been initialized...");
       printf ("There are : %d Elements in the array \n", *filelength);
       printf ("I have %d Workers ready to work \n", numtasks);
- 
+
       workperNode = (*filelength / numtasks);
+
       workLeft = (*filelength % numtasks);
       printf ("Each node will recieve %d elements to search \n", workperNode);
       printf ("With %d left over \n", workLeft);
-      
+
       int i, start;
 
       offset = workperNode;
@@ -72,32 +83,65 @@ int l=0;
 
       for (dest = 1; dest < numtasks; dest++)
   {
-    printf ("offset : %d end: %d \n", offset, (workperNode+offset));
+    printf ("offset : %d end: %d \n", offset, (workperNode + offset));
+    MPI_Send (&lines, 1, MPI_CHAR, dest, 9, MPI_COMM_WORLD);
 
-        MPI_Send (&workperNode, 1, MPI_INT, dest, 3, MPI_COMM_WORLD);
-        MPI_Send (&offset, 1, MPI_INT, dest, 4, MPI_COMM_WORLD);
-        MPI_Send(&(lines[offset][0]), workperNode, MPI_CHAR, dest, tag2,
-                 MPI_COMM_WORLD);
-        printf("Sent Process: %d array \n",dest);      
-     offset = offset + workperNode ;
+    MPI_Send (&workperNode, 1, MPI_INT, dest, 3, MPI_COMM_WORLD);
+    MPI_Send (&offset, 1, MPI_INT, dest, 4, MPI_COMM_WORLD);
+    MPI_Send (&(lines[offset][0]), workperNode, MPI_CHAR, dest, tag2,
+        MPI_COMM_WORLD);
+    printf ("Sent Process: %d array \n", dest);
+    offset = offset + workperNode;
   }
 
 //Call this below when done writing program... 
 //freeMem(* filelength,lines); // Deallocate the memory
- 
+
 
       //////////////////MASTER WORK///////////////////
-      printf("MASTER NODE SCANNING:\n");
+      printf ("MASTER NODE SCANNING:\n");
 
       offset = 0;
+
+      int j = 0;
       for (i = 0; i < workperNode; i++)
   {
     if (scanarray (&lines[i], sizeof (lines[i])) != "not found")
       {
         printf ("%s \n", scanarray (&lines[i], sizeof (lines[i])));
+        outarr[j] = scanarray (&lines[i], sizeof (lines[i]));
+        j++;
       }
 
-}
+
+  }
+      createlogFile (outarr, j);
+
+
+      //char ** masterarr = malloc(sizeof(char *) *workperNode);
+      MPI_Barrier (MPI_COMM_WORLD);
+
+      for (i = 1; i < numtasks; i++)
+  {
+    source = i;
+    MPI_Recv (&lenoflogFile, 1, MPI_INT, source, 10,
+        MPI_COMM_WORLD, &status);
+
+    if (lenoflogFile > 0)
+      {
+
+        MPI_Recv (&(outarr[0][0]), workperNode, MPI_CHAR, source, 5,
+      MPI_COMM_WORLD, &status);
+
+        createlogFile (outarr, lenoflogFile);
+      }
+
+  }
+
+
+
+
+
     }
 
 
@@ -106,26 +150,55 @@ int l=0;
 
   if (taskid > MASTER_NODE_RANK)
     {
+
       source = MASTER_NODE_RANK;
+
+      MPI_Recv (&lines, 1, MPI_CHAR, MASTER_NODE_RANK, 9, MPI_COMM_WORLD,
+    &status);
+
 //This will send the buffer size, since the master created it.
       //Recieve the data from the master.
       MPI_Recv (&workperNode, 1, MPI_INT, source, 3, MPI_COMM_WORLD, &status);
       MPI_Recv (&offset, 1, MPI_INT, source, 4, MPI_COMM_WORLD, &status);
-           printf ("PROCESS %d :offset : %d end: %d \n",taskid, offset, (workperNode+offset));
-      MPI_Recv (&(lines[offset][0]), workperNode , MPI_CHAR, source, tag2,MPI_COMM_WORLD, &status);
-           printf("PROCESS %d recieved the array........\n", taskid);
+      printf ("PROCESS %d :offset : %d end: %d \n", taskid, offset,
+        (workperNode + offset));
 
-int i;
-printf("PROCESS %d Scanning in progress.....\n", taskid);
- for (i = offset; i < (workperNode + offset); i++)
+      MPI_Recv (&(lines[offset][0]), workperNode, MPI_CHAR, source, tag2,
+    MPI_COMM_WORLD, &status);
+      printf ("PROCESS %d recieved the array........\n", taskid);
+
+      int i;
+      printf ("PROCESS %d Scanning in progress.....\n", taskid);
+      int j = 0;
+      for (i = offset; i < (workperNode + offset); i++)
   {
     if (scanarray (&lines[i], sizeof (lines[i])) != "not found")
       {
         printf ("%s \n", scanarray (&lines[i], sizeof (lines[i])));
+        outarr[j] = malloc (300);
+        outarr[j] = scanarray (&lines[i], sizeof (lines[i]));
+        j++;
       }
+
+
+  }
+      //printf("DONE");
+      //printf("\nPOINTER : %p\n",&outarr);
+      dest = MASTER_NODE_RANK;
+      //printf("ARR:%s",outarr[1]);
+      MPI_Barrier (MPI_COMM_WORLD);
+      lenoflogFile = j;
+      MPI_Send (&lenoflogFile, 1, MPI_INT, dest, 10, MPI_COMM_WORLD);
+
+      if (lenoflogFile > 0)
+  {
+    MPI_Send (&(outarr[0][0]), workperNode, MPI_CHAR, dest, 5,
+        MPI_COMM_WORLD);
   }
 
+
     }
+
   MPI_Finalize ();
 }       // End main function
 
@@ -196,6 +269,7 @@ createArray (FILE * logFile, char **lines, int *filelength)
   for (i = 0; i < filelen; i++)
     {
       lines[i] = malloc (sizeof (char *) * maxlinelen); //Allocate each line with the maximum line size, to ensure no data gets removed.
+
       fgets (lines[i], 300, (FILE *) logFile);  //Read from that array that was just created.
     }
 
@@ -229,7 +303,8 @@ printArray (char **lines, int filelength)
 }
 
 
-char * scanarray (char **array, int length)
+char *
+scanarray (char **array, int length)
 {
   int i;
   char *searcher;   // this is what searches for the strings
@@ -244,3 +319,40 @@ char * scanarray (char **array, int length)
 
 }
 
+void
+allocchar ()
+{
+  int i = 0;
+  for (i = 0; i < 200; i++)
+    outarr[i] = malloc (500);
+
+}
+
+void
+deleteoldLog ()
+{
+
+
+  FILE *fileExists;   // Remove old file if it exists
+  if (fileExists = fopen ("log.txt", "r"))
+    {
+      remove ("log.txt");
+    }
+}
+
+
+void
+createlogFile (char **outarr, int lenoflogFile)
+{
+  int i;
+
+
+  FILE *fptr = fopen ("log.txt", "a");
+  for (i = 0; i < lenoflogFile; i++)
+    {
+      fprintf (fptr, "%s\n", outarr[i]);
+    }
+  fclose (fptr);
+
+
+}
